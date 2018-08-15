693ca45183b5176f7543c721c892fa90993a0cb6
Tyrone Thompson <tyrone.thompson@gmail.com>
2018-08-15 16:11:37 -0400

    Middle of HTTP tot

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,21 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
 
-import { Observable, of } from 'rxjs';
+import { Observable, of } from "rxjs";
 
-import { MessageService } from './message.service';
+import { MessageService } from "./message.service";
+
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HeroService {
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 }
